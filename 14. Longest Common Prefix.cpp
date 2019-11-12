ef80d38b51a34a4c265c8b4dd0ee57774b742d2b
Abu Ghalib <33085535+abu112@users.noreply.github.com>
2019-11-12 20:17:06 +0530

    Create 14. Longest Common Prefix.cpp

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+int main() {
+
+	std::vector<std::string> lst1; //Use can use array instead of vector but I like it more.
+
+	//lst1 = {"flower", "flow", "flight"};
+	lst1 = { "dog", "racecar", "car" };
+	char quot= '"';
+	std::string common = "";
+	
+	int t1 = lst1[0].length(); int t2 = lst1[1].length(); //This has been done to reduce the time complexity, instead of using for loop.
+
+	int min = fmin(lst1[2].length(), fmin(t1, t2));
+
+	for (int i = 0; i < min; ++i) {
+		if (lst1[0][i] == lst1[1][i] and lst1[0][i] == lst1[2][i]) common += lst1[0][i];
+		else break;
+	}
+
+	std::cout << quot << common << quot;
+
+	return 0;
+}
